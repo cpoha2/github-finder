3cabeccc8f44b6ccb55c24ab91dbdd77911ac65a
cpoha2 <cpoha2@gmail.com>
2021-05-17 15:19:27 -0400

    End lecture 34 - not found and no results

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+import Home from './components/pages/Home';
+
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
-import Search from './components/users/Search';
+
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
@@ -16,29 +18,6 @@ import './App.css';
 const App = () => {
 
   
-  const [alert, setAlert]  = useState(null);
-
-
-  //async componentDidMount() {
-  //  this.setState({loading: true});
-  //  const res = await axios.get(`https://api.github.com/users?client_id=$
-  //  {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-  //  {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  //  this.setState({users: res.data, loading: false});
-  //}
-
-  
-
-  
-
-  const showAlert = (msg, type) => {
-
-    setAlert({msg, type});
-
-    setTimeout(() => setAlert( null, 5000));
-  } 
-
-  
     return (
       <GithubState>
         <AlertState>
@@ -46,18 +25,12 @@ const App = () => {
         <div className='App'>
           <Navbar />
           <div className='container'>
-            <Alert alert={alert} />
+            <Alert />
             <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search 
-                      setAlert={showAlert}/>
-                  <Users />
-                </Fragment>
-              )} />
+              <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About}/>
               <Route exact path='/user/:login' component={User} />
-              
+              <Route component={NotFound} />
             </Switch>
             
           </div>
